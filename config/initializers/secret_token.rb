481e732ba2b756b6c1eb77ea06a77081afec56d0
Ryan Ong <ryanong@gmail.com>
2012-05-21 21:35:59 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Radiator::Application.config.secret_token = '0725b82923e16f77973c2ed0dadfc2a7a8871290aa388f2b460ee5692581ba1b38dbf34638bcaef38e27a898febdc1d11cc8750f7926b6fec91b1eb3f979dae2'
